3161957328a30411795fa72af583e44691b92104
Gerald Ezenagu <egerald344@gmail.com>
2022-07-27 23:55:24 +0100

    working on file structure

--- gt3-modernUI/src/App.js
+++ gt3-modernUI/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import {Article, Brand, CTA} from './components';
+
 const App = () => {
     return ( 
         <div>
-            <h2>GT-3</h2>
+            <h2>GpT-3</h2>
         </div>
      );
 }
