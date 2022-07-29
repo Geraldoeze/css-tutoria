b5d0a0c92537866b9eb6fc7e864bfe0f8b97f9a5
Gerald Ezenagu <egerald344@gmail.com>
2022-07-29 03:04:20 +0100

    working on features

--- gt3-modernUI/src/components/feature/Feature.jsx
+++ gt3-modernUI/src/components/feature/Feature.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+const Feature = (props) => {
     return ( 
-        <div>
-            Feature
+        <div className='gpt3__features-container__feature'>
+           <div className='gpt3__features-container__feature-title'>
+               <div />
+               <h1>{props.title}</h1>
+           </div>
+           <div className='gpt3__features-container_feature-text'>
+            {props.text}
+           </div>
+            
         </div>
      );
 }
--- gt3-modernUI/src/container/whatGPT3/WhatGPT3.jsx
+++ gt3-modernUI/src/container/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>Baler</div>
+        <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature title='What is GPT-3' text='lorem cjdkjbceruq rei berjc r ekbvefaikne rqvkcnkdLCBU;NO   F'/>
+            </div>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>
+                    The Possibilities are beyound your imagination
+                </h1>
+                <p>Explore the library</p>
+            </div>
+            <div className='gpt3__whatgpt3-container'>
+                <Feature title="Chatbots" text="the gates of posedium valley of the sea and watger rgon in the continen of the world" />
+                <Feature title="Knowledgebase" text="the gates of posedium valley of the sea and watger rgon in the continen of the world" />
+                <Feature title="Education" text="the gates of posedium valley of the sea and watger rgon in the continen of the world" />
+            </div>
+        </div>
     );
 }
  
