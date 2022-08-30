dd4fecc2e195307af142380aa7fc7b1a9ee7dd09
Gerald Ezenagu <egerald344@gmail.com>
2022-08-30 21:51:54 +0100

    working on styles

--- real-a/components/SearchFilters.jsx
+++ real-a/components/SearchFilters.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import { MdCancel } from 'react-icons/md';
-import Image from 'next/image';
+// import { useRouter } from 'next/router';
+// import { MdCancel } from 'react-icons/md';
+// import Image from 'next/image';
 
 import { filterData, getFilterValues } from '../utils/filterData';
 
